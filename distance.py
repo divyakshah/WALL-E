a31abfd105b6d5b96ae20e2e405f8883d48a5dd7
divyakshah <96028996+divyakshah@users.noreply.github.com>
2021-12-12 19:09:46 -0500

    Create distance.py

---
+++ distance.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import board
+import busio
+import time
+import adafruit_vl53l0x
+
+#Initialize I2C bus and sensor
+i2c = busio.I2C(board.SCL, board.SDA)
+vl53 = adafruit_vl53l0x.VL53L0X(i2c)
+
+#Main Loop will read the range and print it every second. 
+while True:
+   print("Range: {0}mm".format(vl53.range))
+   time.sleep(0.1)
+  
